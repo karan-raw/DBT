16b701a478f49f70b24c99c4512c84f031bf9490
Karan DC <139109082+karan-raw@users.noreply.github.com>
2024-01-16 14:50:39 +0530

    Update model1.sql

--- models/model1.sql
+++ models/model1.sql
@@ -7,4 +7,4 @@
 -- select temp1."row_id","order_id", "order_date", "return_request_date","item_name" from dc_dc_frontendtest_006.asc_bb_returns_report_by_return_date_kr as TEMP1 full outer join 
 -- dc_dc_frontendtest_006.asc_bb_seller_performance_report_kr as TEMP2 on TEMP1."row_id"=TEMP2."row_id
 
-select "{var('abcd')}" from dc_dc_frontendtest_003.lnads_srb_campaign_insights_kr
+select "{{ var('abcd') }}" from dc_dc_frontendtest_003.lnads_srb_campaign_insights_kr
